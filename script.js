34ff786929b7befd6148789e05e25955fbc4f561
nsamedaisy <nsamedaisy@gmail.com>
2023-04-19 10:56:59 +0100

    fixed indentation on css, and js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let colorInput = document.querySelector('#color');
 let hexInput = document.querySelector('#hex');
 
 colorInput.addEventListener('input', () => {
-	let color = colorInput.value;
-	hexInput.value = color;
-	document.body.style.backgroundColor = color;
+  let color = colorInput.value;
+  hexInput.value = color;
+  document.body.style.backgroundColor = color;
 });
